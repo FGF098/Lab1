4fbc96c2357ce707f24b10139795931e03c276ab
Олесь Баженов <bazhenov.095@gmail.com>
2020-12-19 01:53:38 +0200

    added HoursShift

---
+++ Lab1/HoursShift.cpp
@@ -0,0 +1,33 @@
+#include "HoursShift.h"
+
+int HoursShift::getDays() {
+	return days;
+}
+
+int HoursShift::getHours() {
+	return hours;
+}
+
+HoursShift::HoursShift(int hours_shift, int additional_minutes, int timeZoneHours) {
+	hours = hours_shift + additional_minutes + timeZoneHours;
+	Optimize();
+}
+
+HoursShift::HoursShift(int reduction, int subtractor, int additional_hours, int timeZoneHours) {
+	hours = reduction - subtractor + additional_hours + timeZoneHours;
+	Optimize();
+}
+
+void HoursShift::Optimize() {
+	int hourRes, dayRes;
+	if (hours >= 0) {
+		dayRes = hours / HOURS_IN_DAY;
+		hourRes = hours % HOURS_IN_DAY;
+	}
+	else {
+		dayRes = hours / HOURS_IN_DAY - 1;
+		hourRes = HOURS_IN_DAY - hours % HOURS_IN_DAY;
+	}
+	hours = hourRes;
+	days = dayRes;
+}
---
+++ Lab1/HoursShift.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "Time.h"
+#include "MinutesShift.h"
+#include "TimeZoneShift.h"
+
+class HoursShift {
+
+public:
+
+	/// <returns>hours shift</returns>
+	int getHours();
+
+	/// <returns>extended days</returns>
+	int getDays();
+
+	/// <summary>
+	/// Constuctor for substraction, optimizing when callling
+	/// </summary>
+	/// <param name="reduction"></param>
+	/// <param name="subtractor"></param>
+	/// <param name="additional hours from minutes shift"></param>
+	/// <param name="hours from time zone shift"></param>
+	HoursShift(int reduction, int subtractor, int additional_hours, int timeZoneHours);
+
+	/// <summary>
+	/// Constructor for custom creating
+	/// </summary>
+	/// <param name="hours shift"></param>
+	/// <param name="additional hours from minutes shift"></param>
+	/// <param name="hours from time zone shift"></param>
+	HoursShift(int hours_shift, int additional_minutes, int timeZoneHours);
+
+private:
+
+	int hours;
+
+	int days = 0;
+
+	/// <summary>
+	/// optimized hours will be positive
+	/// </summary>
+	void Optimize();
+
+};
