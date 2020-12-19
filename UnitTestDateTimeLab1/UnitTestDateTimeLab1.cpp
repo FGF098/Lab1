a0fcf0b91fe9dc57a9592a139702b1a2c3519bab
Олесь Баженов <bazhenov.095@gmail.com>
2020-12-19 12:18:36 +0200

    added test progect

---
+++ UnitTestDateTimeLab1/UnitTestDateTimeLab1.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestDateTimeLab1
+{
+	TEST_CLASS(UnitTestDateTimeLab1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
